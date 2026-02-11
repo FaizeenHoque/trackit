e15371211318c38a2ccdc413f9cb276cef811c5e
FaizeenHoque <haquefaizeen11@gmail.com>
2026-02-11 08:19:35 +0600

    Added HackClub logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import HackClubLogo from "./components/Icons";
 import PixelEarth from "./components/PixelEarth";
 
 export default function Home() {
   return (
     <div>
+      
       <PixelEarth />
     </div>
   );
